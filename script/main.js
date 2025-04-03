15f81fd69813d43c7a658b172f593069508c34c1
spctrm404 <46218112+spctrm404@users.noreply.github.com>
2025-04-03 11:02:18 +0900

    add timer func

--- script/main.js
+++ script/main.js
@@ -2,18 +2,42 @@ import { urls } from './urls.js';
 
 let cnt = 0;
 const iFrame = document.querySelector('#iframe');
-iFrame.src = `https://${urls[cnt]}`;
 const next = document.querySelector('#next');
-next.addEventListener('click', () => {
-  cnt++;
-  if (cnt >= urls.length) cnt = 0;
-  console.log(`URL: ${urls[cnt]}`);
-  iFrame.src = `https://${urls[cnt]}`;
-});
 const prev = document.querySelector('#prev');
-prev.addEventListener('click', () => {
-  cnt--;
-  if (cnt < 0) cnt = urls.length - 1;
-  console.log(`URL: ${urls[cnt]}`);
+
+function updateIframe() {
   iFrame.src = `https://${urls[cnt]}`;
-});
+  console.log(`URL: ${urls[cnt]}`);
+}
+
+// 다음 URL로 이동
+function goNext() {
+  cnt = (cnt + 1) % urls.length;
+  updateIframe();
+  resetTimer();
+}
+
+// 이전 URL로 이동
+function goPrev() {
+  cnt = (cnt - 1 + urls.length) % urls.length;
+  updateIframe();
+  resetTimer();
+}
+
+// 타이머 관련
+let timer = null;
+function startTimer() {
+  timer = setTimeout(goNext, 180 * 1000); // 3분 후 자동으로 다음 URL
+}
+function resetTimer() {
+  clearTimeout(timer);
+  startTimer();
+}
+
+// 이벤트 핸들러 등록
+next.addEventListener('click', goNext);
+prev.addEventListener('click', goPrev);
+
+// 초기화
+updateIframe();
+startTimer();
